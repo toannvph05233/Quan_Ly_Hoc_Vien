a4242ca1354286b27fee1eac49910c090f12527b
toannvph05233 <johntoan98@gmail.com>
2023-06-01 09:05:49 +0700

    up luu DL

--- managerNumber.js
+++ managerNumber.js
@@ -1,10 +1,9 @@
-let quan = new HocVien("Ny Quan", 15, "https://openseauserdata.com/files/02d992a4c7039936aea0bce0ec7a5e1e.jpg");
-let khanh = new HocVien("Ny Khánh", 18, "https://giadinh.mediacdn.vn/296230595582509056/2022/2/17/27212817110061775266623491256080737665900375n-16450863187391857196552.jpg");
-
-let hocviens = [quan,khanh];
+let hocviens = JSON.parse(localStorage.getItem("hocviens"));
 let indexEdit = -1;
 
-function show() {
+function showManager() {
+    hocviens = JSON.parse(localStorage.getItem("hocviens"));
+    console.log(hocviens);
     let str = '';
     for (let i = 0; i < hocviens.length; i++) {
         str += `<tr>
@@ -13,6 +12,22 @@ function show() {
                     <td><img src="${hocviens[i].img}" width="150" height="200"></td>
                     <td><button onclick="showEdit(${i})">Edit</button></td>
                     <td><button onclick="deleteNumber(${i})">Delete</button></td>
+                 </tr>`
+    }
+
+    let element = document.getElementById("display");
+    element.innerHTML = str;
+
+}
+
+function showHome() {
+    hocviens = JSON.parse(localStorage.getItem("hocviens"));
+    let str = '';
+    for (let i = 0; i < hocviens.length; i++) {
+        str += `<tr>
+                    <td>${hocviens[i].name}</td>
+                    <td>${hocviens[i].age}</td>
+                    <td><img src="${hocviens[i].img}" width="150" height="200"></td>
                  </tr>`
     }
 
@@ -36,12 +51,14 @@ function add() {
         hocviens[indexEdit] = newObj;
         indexEdit = -1;
     }
-    show();
+    localStorage.setItem("hocviens", JSON.stringify(hocviens));
+    showManager();
 }
 
 function deleteNumber(index) {
     hocviens.splice(index, 1);
-    show();
+    localStorage.setItem("hocviens", JSON.stringify(hocviens));
+    showManager();
 }
 
 function showEdit(index) {
@@ -63,4 +80,3 @@ function checkName() {
 
 }
 
-show();
